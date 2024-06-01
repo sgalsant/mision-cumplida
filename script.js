d7e6d229bef65d56ba2a9cb6f3eec54ec866ca91
santiago galván <sgalsant@gmail.com>
2024-06-01 14:11:09 +0100

    Update script.js

--- script.js
+++ script.js
@@ -69,7 +69,7 @@ document.addEventListener('DOMContentLoaded', () => {
     }
 
     function updateCardCounter() {
-        cardCounter.innerText = `Misiones cumplidas: ${cardCount} - ${descriptions.length}`;
+        cardCounter.innerText = `Misiones cumplidas: ${cardCount} - santiago`;
   /*      if (descriptions.length == 0) {
             alert ("enhorabuena has completado " + cardCount + " misiones" );
         }
